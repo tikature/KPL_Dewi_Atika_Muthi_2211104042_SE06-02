2dafac250f5cd997cdb4521937d42cbb5dbeb51d
tikature <detikaa10@gmail.com>
2025-03-13 08:41:13 +0700

    UP 1

---
+++ praktikum_1/readfile.js
@@ -0,0 +1,5 @@
+
+const fs = require('fs');
+ 
+fs.writeFileSync('hello.txt', 'Hello, girlie!');
+console.log('File telah dibuat.');
---
+++ praktikum_1/var.js
@@ -0,0 +1,24 @@
+// aritmatika
+i = 5;
+y = 8;
+z = i * y;
+
+console.log(z);
+
+// sintaks
+let x;
+x = 6;
+
+console.log(x);
+
+// konstanta
+// You can create a constant array:
+const fruits = ["Jeruk", "Belimbing", "Markisa"];
+
+// You can change an element:
+fruits[0] = "Apel";
+
+// // You can add an element:
+fruits.push();
+
+console.log(fruits);
